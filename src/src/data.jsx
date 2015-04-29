31b0f6e1f9a5149d3efa5ac19ffc570ac4034557
Tom Rees <zephod@gmail.com>
2015-04-29 17:07:29 +0100

    Remove old fake code which breaks things.

--- src/src/data.jsx
+++ src/src/data.jsx
@@ -16,56 +16,6 @@ var raw_data = {
         '#006d2c',
         '#00441b',
     ],
-    data: {
-        'E01004376': {
-            geometry: require('../oa_by_lsoa/E01004376.json'),
-            rent: 450,
-            crime: 0,
-            transport: max,
-            green_space: max,
-            schools: 0,
-        },
-        'E01004375': {
-            geometry: require('../oa_by_lsoa/E01004375.json'),
-            rent: 350,
-            crime:       0.2,
-            transport:   max,
-            green_space: 0.2,
-            schools:     0.1,
-        },
-        'E01004374': {
-            geometry: require('../oa_by_lsoa/E01004374.json'),
-            rent: 250,
-            crime:       0.1,
-            transport:   0.1,
-            green_space: max,
-            schools:     0.2,
-        },
-        'E01004373': {
-            geometry: require('../oa_by_lsoa/E01004373.json'),
-            rent:    200,
-            crime:       max,
-            transport:   0,
-            green_space: max,
-            schools:     0.3,
-        },
-        'E01004372': {
-            geometry: require('../oa_by_lsoa/E01004372.json'),
-            rent: 200,
-            crime:       0,
-            transport:   0,
-            green_space: 0,
-            schools:     max,
-        },
-        'E01004371': {
-            geometry: require('../oa_by_lsoa/E01004371.json'),
-            rent: 150,
-            crime:       0.2,
-            transport:   0,
-            green_space: 0,
-            schools:     0,
-        }
-    },
     cards: {
        	1: {
             id: 1,
