6cc598ca24b500fd9973a675e99d049cd4fd98ac
Tom Rees <zephod@gmail.com>
2015-07-06 02:06:54 +0100

    Frontend design work

---
+++ src/mycity.js
@@ -0,0 +1,144 @@
+window.MyCity = {
+    plugin: {
+        onClickOA: function(oa, lsoa) {
+            // alert('Clicked OA: ' + oa + ', LSOA: ' + lsoa);
+            console.log("onClickOA",oa,lsoa);
+            // popup.innerHTML = 'OA <a href="javascript: MyCity.plugin.close()">x</a>';
+            popup.style("right","0%");
+        },
+        onReceiveData: function(data) {
+            var summaryData;
+            if (window.summary!=undefined && window.summary[data.oa]) {
+              summaryData = window.summary[data.oa];
+            }
+            else {
+              summaryData = {
+                "transport"   : -1,
+                "schools"     : -1,
+                "green_space" : -1,
+                "safety"      : -1,
+              };
+            }
+            // console.log("got summary data",summaryData);
+            function scoreWord(val) {
+              // TODO what are the actual thresholds?
+              if (val<0)    { return "Unknown"; }
+              if (val<0.4)  { return "Below Average"; }
+              if (val<0.6)  { return "Average"; }
+              if (val<0.8)  { return "Above Average"; }
+              if (val<=1.0) { return "Excellent"; }
+              return val;
+            }
+            function scoreNumber(val) {
+              // TODO what are the actual thresholds?
+              if (val<0)    { return 0; }
+              if (val<0.4)  { return 1; }
+              if (val<0.6)  { return 2; }
+              if (val<0.8)  { return 3; }
+              if (val<=1.0) { return 4; }
+              return 0;
+            }
+
+
+            // ====
+            popup.select('.box-rent h3').html("<span class=\"color\">"+data.postcode+":</span> Cost Per Month");
+            var graph_rent = popup.select('.box-rent .graph');
+            graph_rent.html( "<pre>"+JSON.stringify(data.rent)+"</pre>" );
+            // ====
+            popup.select('.box-schools h3').html("Overall: <span class=\"color\">"+scoreWord(summaryData.schools)+"</span>");
+            var graph_schools = popup.select('.box-schools .graph');
+            graph_schools.html( "<pre>"+JSON.stringify(data.schools)+"</pre>" );
+            // ====
+            popup.select('.box-transport h3').html("Overall: <span class=\"color\">"+scoreWord(summaryData.transport)+"</span>");
+            var graph_transport = popup.select('.box-transport .graph');
+            graph_transport.html( "<pre>Zone "+JSON.stringify(data.fareZone)+"</pre>" +
+                           "<pre>"+JSON.stringify(data.timeToBank)+"</pre>");
+            // ====
+            popup.select('.box-greenspace h3').html("Overall: <span class=\"color\">"+scoreWord(summaryData.green_space)+"</span>");
+            var graph_greenspace = popup.select('.box-greenspace .graph');
+            graph_greenspace.html( "<pre>Score: Unknown</pre>");
+            // ====
+            popup.select('.box-safety h3').html("Overall: <span class=\"color\">"+scoreWord(summaryData.safety)+"</span>");
+            var safety_n = scoreNumber(summaryData.safety);
+            console.log(safety_n);
+            safety_n = 1+Math.floor(Math.random()*4);
+
+            var graph_safety = popup.select('.box-safety .graph');
+            graph_safety.selectAll(".clip").data([safety_n])
+              .style("width",function(d) { return (d*25)+"%"})
+              .style("border-color",function(d) { return d==4 ? "transparent" : "#fff"; });
+            // ====
+            popup.select('.box-information h3').html("Postcode: <span class=\"color\">"+data.postcode+"</span> &nbsp;Area: <span class=\"color\">"+data.oa+"</span>");
+            var graph_information = popup.select('.box-information .graph');
+            console.log(data.areaDescription);
+            graph_information.html("<strong>Local Area:</strong><blockquote>"+data.areaDescription['OA_description']+"</blockquote><strong>Regional Description:</strong><blockquote>"+data.areaDescription['General_description']+"</blockquote>");
+        },
+        close: function() {
+            popup.style("right","-45%");
+            MyCity.close();
+        }
+    }
+};
+
+// Set up global namespace for popup
+window.popup = d3.select("#mycity-popup");
+popup.html('\
+  <div class="main-logo">\
+    <div class="line line-left"></div>\
+    <div class="line line-right"></div>\
+    <h1 class="middle">\
+      INFORMATION\
+      <a class="close-button" href="javascript:MyCity.plugin.close()">&times</a>\
+    </h1>\
+  </div>\
+  <div class=\"box-table\">\
+    <div class=\"box-row\">\
+      <div class="box box-rent">\
+        <H2>RENT</H2>\
+        <h3></h3>\
+        <div class="graph"></div>\
+      </div>\
+      <div class="box box-schools">\
+        <H2>SCHOOLS</H2>\
+        <h3></h3>\
+        <div class="graph"></div>\
+      </div>\
+    </div>\
+    <div class=\"box-row\">\
+      <div class="box box-transport">\
+        <H2>TRANSPORT</H2>\
+        <h3></h3>\
+        <div class="graph"></div>\
+      </div>\
+      <div class="box box-greenspace">\
+        <H2>GREEN SPACE</H2>\
+        <h3></h3>\
+        <div class="graph"></div>\
+      </div>\
+    </div>\
+    <div class=\"box-row\">\
+      <div class="box box-safety">\
+        <H2>SAFETY</H2>\
+        <h3></h3>\
+        <div class="graph">\
+          <div class="safety-graph">\
+            <div class="noclip">\
+              <img src="/http/art/safety-bg.png" />\
+            </div>\
+            <div class="clip">\
+              <img src="/http/art/safety.png" />\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div class="box box-information">\
+        <H2>AREA INFORMATION</H2>\
+        <h3></h3>\
+        <div class="graph"></div>\
+      </div>\
+    </div>\
+  </div>\
+');
+
+
+
--- src/src/index.jsx
+++ src/src/index.jsx
@@ -24,6 +24,15 @@ Cheating on:
 
 */
 
+var resource_ids = {
+  "bbox"                                   : "d62ad9f3-4a5d-410b-993a-134c57ce52ee",
+  "modelled_OA_rents"                      : "c16db584-f212-439f-b735-507fe6c49955",
+  "MyLondon_traveltime_to_Bank_station_OA" : "c2e9ebc1-935b-460c-9361-293398d84fe5",
+  "MyLondon_postcode_OA"                   : "94baeb22-9d31-4332-95ab-84d4aaf72f22",
+  "MyLondon_LOAC_area_description_text_v3" : "3848d6af-bd5e-4317-8676-bbb857f773e0",
+  "MyLondon_fare_zone_OA"                  : "5ff9ce59-2a77-456a-9ecc-f8fb80660ef1",
+  "MyLondonSchoolsCatchmentv2"             : "98ec0962-af1a-49ad-ac10-47606f7794da"
+}
 
 var L = require('leaflet');
 var d3 = require('d3');
@@ -111,7 +120,7 @@ var getTimeToBankFromOA = function(oa) {
     return new Promise(function (resolve, reject) {
         request.get('http://api.datapress.io/api/3/action/datastore_search')
         .query({
-            resource_id: 'MyLondon_traveltime_to_Bank_station_OA',
+            resource_id: resource_ids['MyLondon_traveltime_to_Bank_station_OA'],
             filters: JSON.stringify({OA11CD: oa})
         })
         // .withCredentials()
@@ -142,7 +151,7 @@ var getPostcodeFromOA = function(oa) {
     return new Promise(function (resolve, reject) {
         request.get('http://api.datapress.io/api/3/action/datastore_search')
         .query({
-            resource_id: 'MyLondon_postcode_OA',
+            resource_id: resource_ids['MyLondon_postcode_OA'],
             filters: JSON.stringify({OA11CD: oa})
         })
         .end(function (err, res) {
@@ -172,7 +181,7 @@ var getAreaDescriptionFromOA = function(oa) {
     return new Promise(function (resolve, reject) {
         request.get('http://api.datapress.io/api/3/action/datastore_search')
         .query({
-            resource_id: 'MyLondon_LOAC_area_description_text_v3',
+            resource_id: resource_ids['MyLondon_LOAC_area_description_text_v3'],
             filters: JSON.stringify({OA11CD: oa})
         })
         .end(function (err, res) {
@@ -201,7 +210,7 @@ var getGeospatialFromOA = function(oa) {
     return new Promise(function (resolve, reject) {
         request.get('http://api.datapress.io/api/3/action/datastore_search')
         .query({
-            resource_id: 'bbox',
+            resource_id: resource_ids['bbox'],
             filters: JSON.stringify({oa: oa})
         })
         .end(function (err, res) {
@@ -231,7 +240,7 @@ var getRentFromOA = function(oa) {
     return new Promise(function (resolve, reject) {
         request.get('http://api.datapress.io/api/3/action/datastore_search')
         .query({
-            resource_id: 'modelled_OA_rents',
+            resource_id: resource_ids['modelled_OA_rents'],
             filters: JSON.stringify({OA11CD: oa})
         })
         .end(function (err, res) {
@@ -260,7 +269,7 @@ var getFareZoneFromOA = function(oa) {
     return new Promise(function (resolve, reject) {
         request.get('http://api.datapress.io/api/3/action/datastore_search')
         .query({
-            resource_id: 'MyLondon_fare_zone_OA',
+            resource_id: resource_ids['MyLondon_fare_zone_OA'],
             filters: JSON.stringify({OA11CD: oa})
         })
         .end(function (err, res) {
@@ -289,7 +298,7 @@ var getSchoolsFromOA = function(oa) {
     return new Promise(function (resolve, reject) {
         request.get('http://api.datapress.io/api/3/action/datastore_search')
         .query({
-            resource_id: 'MyLondonSchoolsCatchmentv2',
+            resource_id: resource_ids['MyLondonSchoolsCatchmentv2'],
             filters: JSON.stringify({OA11CD: oa})
         })
         .end(function (err, res) {
@@ -403,6 +412,7 @@ var getSummary = function() {
                         schools: parseFloat(parts[7]),
                     };
                 }
+                window.summary = summary;
                 resolve({
                     summary: summary,
                     lsoa_to_oa: lsoa_to_oa,
@@ -1366,7 +1376,7 @@ var Main = React.createClass({
                 this.context.router.transitionTo('map', this.props.params, query);
                 MyCity.oa = undefined;
             }.bind(this);
-            PopupDataFetcher.getData(this.props.query.oa)
+            PopupDataFetcher.getData(this.props.query.oa);
         }
     },
 
